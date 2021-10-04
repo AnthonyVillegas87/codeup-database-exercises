6175fe6015999ccb0137ddb563c40b417f7eff88
Anthony Villegas <villegasanthony87@yahoo.com>
2021-10-04 15:16:33 -0500

    feat: added employees db

---
+++ select_exercises.sql
@@ -0,0 +1,24 @@
+USE codeup_test_db;
+
+SELECT album_artist, album_name FROM albums
+WHERE album_release_date< 1980;
+
+#Name of all albums by Pink Floyd
+SELECT album_id, album_artist, album_name FROM albums
+WHERE album_artist = 'Pink Floyd';
+#The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT album_release_date FROM albums
+WHERE album_name = 'Sgt. Peppers Lonely Hearts Club Band';
+#The genre for Nevermind
+SELECT album_genre From albums
+WHERE album_name = 'Nevermind';
+#Which albums were released in the 1990s
+SELECT album_release_date, album_name, album_artist From albums
+WHERE album_release_date BETWEEN  '1990' AND '1999';
+#Which albums had less than 20 million certified sales
+SELECT album_name, album_sales FROM albums
+WHERE album_sales <= '20';
+#All the albums with a genre of "Rock".
+# Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT album_name, album_genre FROM albums
+WHERE album_genre = 'Rock';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,22 +1,28 @@
 USE codeup_test_db;
+#All albums in your table
 
-SELECT album_artist, album_name FROM albums
-WHERE album_release_date< 1980;
+SELECT album_name, album_sales FROM albums;
+UPDATE albums
+SET album_sales = (album_sales * 10);
 
-#Name of all albums by Pink Floyd
-SELECT album_id, album_artist, album_name FROM albums
-WHERE album_artist = 'Pink Floyd';
-#The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT album_release_date FROM albums
-WHERE album_name = 'Sgt. Peppers Lonely Hearts Club Band';
-#The genre for Nevermind
-SELECT album_genre From albums
-WHERE album_name = 'Nevermind';
-#Which albums were released in the 1990s
-SELECT album_release_date, album_name, album_artist From albums
-WHERE album_release_date BETWEEN  '1990' AND '1999';
-#Which albums had less than 20 million certified sales
-SELECT album_name, album_sales FROM albums
-WHERE album_sales <= '20';
-#All the albums with a genre of "Rock".
-# Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+
+
+
+#All albums released before 1980
+SELECT album_name, album_release_date FROM albums
+WHERE album_release_date < 1980;
+UPDATE albums
+SET album_release_date = 1800
+WHERE album_release_date < 1979;
+
+
+
+#All albums by Michael Jackson
+SELECT album_name, album_artist FROM albums
+WHERE album_artist = 'Michael Jackson';
+UPDATE albums
+SET album_artist = 'Peter Jackson'
+WHERE album_artist = 'Michael Jackson';
+
+SELECT * FROM albums;
+

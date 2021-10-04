4174dad3a907daff758563ae0376a32e6b773f75
Anthony Villegas <villegasanthony87@yahoo.com>
2021-10-04 12:52:13 -0500

    feat: albums exercise pt 2

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     album_id INT NOT NULL AUTO_INCREMENT,
-    album_artist VARCHAR(35) NOT NULL,
-    album_name VARCHAR(35) NOT NULL,
-    album_release_date DATETIME NOT NULL,
-    album_sales FLOAT NOT NULL,
-    album_genre VARCHAR(35),
+    album_artist VARCHAR(128) NOT NULL,
+    album_name VARCHAR(128) NOT NULL,
+    album_release_date SMALLINT UNSIGNED NOT NULL,
+    album_genre VARCHAR(128) NOT NULL,
+    album_sales FLOAT(6,2),
     PRIMARY KEY(album_id)
 );
+

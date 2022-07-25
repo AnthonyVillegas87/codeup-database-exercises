0dda813f9cf8a05e2a7edbe53508df35d746333e
Anthony Villegas <villegasanthony87@yahoo.com>
2022-07-25 17:50:09 -0500

    feat: data types exercises

--- data_types.sql
+++ data_types.sql
@@ -60,3 +60,61 @@ SELECT birth_date_time, birth_date_time + INTERVAL 5 MONTH FROM people;
 SELECT birth_date_time, birth_date_time - INTERVAL 5 MONTH FROM people;
 SELECT birth_date_time, birth_date_time + INTERVAL 15 MONTH + INTERVAL 10 HOUR FROM people;
 
+# TIMESTAMPS
+CREATE TABLE comments (
+  content VARCHAR(100),
+  created_at TIMESTAMP DEFAULT NOW()
+);
+INSERT INTO comments(content) VALUES('lol what a funny article');
+INSERT INTO comments(content) VALUES ('i found this offensive');
+
+SELECT * FROM comments;
+
+INSERT INTO comments(content) VALUES('wjduyfdubsbhui');
+SELECT * FROM comments;
+
+SELECT * FROM comments ORDER BY created_at;
+SELECT * FROM comments ORDER BY created_at DESC;
+
+CREATE TABLE comments2 (
+    content VARCHAR(100),
+    changed_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO comments2(content) VALUES('lol THIS IS FAKE');
+INSERT INTO comments2(content) VALUES ('Whoa, is this possible???');
+
+SELECT * FROM comments2;
+
+INSERT INTO comments2(content) VALUES ('???');
+
+SELECT * FROM comments2;
+
+UPDATE comments2 SET content = 'THIS IS NOT A TEST' WHERE content = '???';
+SELECT * FROM comments2;
+SELECT * FROM comments2 ORDER BY changed_at;
+
+# EXERCISES
+CREATE TABLE inventory (
+    item_name VARCHAR(100),
+    price DECIMAL(8, 2),
+    quantity INT
+);
+
+SELECT CURTIME() AS 'current time';
+SELECT CURDATE() AS 'current date';
+SELECT DAYOFWEEK(NOW()) AS 'current day of the week';
+SELECT DAYNAME(NOW()) AS 'current day of the week';
+SELECT DATE_FORMAT(NOW(), '%m/%d/%Y at %h:%m');
+SELECT DATE_FORMAT(NOW(), '%M %D at %h:%i');
+
+CREATE TABLE tweets(
+    content VARCHAR(140),
+    username VARCHAR(140),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+
+
+
+

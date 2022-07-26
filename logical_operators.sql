7ad6b2ba253a2d1fa8f9d8afd516e8d33eafffed
Anthony Villegas <villegasanthony87@yahoo.com>
2022-07-26 00:02:17 -0500

    feat: OPERATORS exercises

--- logical_operators.sql
+++ logical_operators.sql
@@ -54,3 +54,14 @@ SELECT title, stock_quantity, CASE
     WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
     ELSE '****' END AS stock
 FROM books;
+
+# EXERCISES
+SELECT title, released_year FROM books WHERE released_year < 1980;
+SELECT title, author_lname FROM books WHERE author_lname = 'Eggers' OR author_lname = 'Chabon';
+SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Lahiri' AND released_year > 2000;
+SELECT * FROM books WHERE pages BETWEEN 100 AND 200 ORDER BY pages;
+SELECT title, author_lname FROM books WHERE author_lname LIKE 'C%' OR author_lname LIKE 'S%';
+
+# CASE EXERCISES
+SELECT title, author_lname, CASE WHEN title LIKE '%stories%' THEN 'Short Stories' WHEN title LIKE '%Just Kids%' OR title LIKE '%A Heartbreaking Work%' THEN 'Memoir' ELSE 'Novel' END AS type FROM books;
+

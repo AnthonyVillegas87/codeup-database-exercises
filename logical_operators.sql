c5cc8ce6684348c2a3d48dad1c794413998c0194
Anthony Villegas <villegasanthony87@yahoo.com>
2022-07-25 21:56:17 -0500

    feat: logical or

--- logical_operators.sql
+++ logical_operators.sql
@@ -20,4 +20,8 @@ SELECT * FROM books WHERE author_lname = 'Eggers' AND books.released_year > 2010
 SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Eggers' AND released_year > 2010;
 SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Eggers' AND released_year > 2010 AND title LIKE '%novel';
 
-# LOGICAL OR
+# LOGICAL OR ||
+SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Eggers' OR released_year > 2010;
+SELECT title, author_lname, released_year, stock_quantity FROM books WHERE author_lname = 'Eggers' OR released_year > 2010 OR stock_quantity > 100;
+
+# BETWEEN

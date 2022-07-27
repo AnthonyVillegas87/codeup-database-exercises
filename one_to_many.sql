8c30940d5e4e7d86b6b7acd1843a4cb324ce6b6a
Anthony Villegas <villegasanthony87@yahoo.com>
2022-07-26 23:34:16 -0500

    feat: JOIN exercises

--- one_to_many.sql
+++ one_to_many.sql
@@ -101,4 +101,37 @@ SELECT * FROM customers;
 SELECT * FROM orders;
 DELETE FROM customers WHERE email = 'george@gmail.com';
 
+#### JOINS EXERCISES ####
+CREATE TABLE students(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100)
+);
 
+CREATE TABLE papers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    grade INT,
+    student_id INT,
+    FOREIGN KEY(student_id) REFERENCES students(id) ON DELETE CASCADE
+);
+
+INSERT INTO students (first_name) VALUES ('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+
+INSERT INTO papers(title, grade, student_id) VALUES
+('My First Book Report', 60, 1),
+('My Second Book Report', 75, 1),
+('Russian Lit Through The Ages', 94, 2),
+('De Montaigne and The Art of The Essay', 98, 2),
+('Borges and Magical Realism', 89, 4);
+
+#### EXPLICIT INNER JOIN ####
+SELECT first_name, title, grade FROM students JOIN papers WHERE students.id = papers.student_id ORDER BY grade DESC;
+
+#### LEFT JOIN ####
+SELECT first_name, title, grade FROM students LEFT JOIN papers ON students.id = papers.student_id;
+SELECT first_name, IFNULL(title, 'missing'), IFNULL(grade, 0) FROM students LEFT JOIN papers ON students.id = papers.student_id;
+
+SELECT first_name, IFNULL(AVG(grade), 0) AS average FROM students LEFT JOIN papers ON students.id = papers.student_id GROUP BY students.id ORDER BY average DESC;
+SELECT first_name, IFNULL(AVG(grade), 0) AS average,
+       CASE WHEN AVG(grade) >= 75 THEN 'passing' ELSE 'failing' END AS passing_status
+FROM students LEFT JOIN papers ON students.id = papers.student_id GROUP BY students.id ORDER BY average DESC;

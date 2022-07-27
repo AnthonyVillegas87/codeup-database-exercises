0a46d68621b019d1a16ac23aee50219a45fed6f1
Anthony Villegas <villegasanthony87@yahoo.com>
2022-07-27 15:53:26 -0500

    feat: follows, tags, & photo tags tables created

--- Social Media/social_media_db.sql
+++ Social Media/social_media_db.sql
@@ -35,6 +35,28 @@ CREATE TABLE likes(
    PRIMARY KEY(user_id, photo_id)
 );
 
+CREATE TABLE follows(
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() ,
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+CREATE TABLE tags(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags(
+    photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
 
 INSERT INTO users(username) VALUES
     ('Jmac'),
@@ -57,6 +79,23 @@ INSERT INTO likes(user_id, photo_id) VALUES
     (1, 2),
     (1, 3),
     (3, 3);
+
+INSERT INTO follows(follower_id, followee_id) VALUES
+    (1, 2),
+    (1, 3),
+    (3, 1),
+    (2, 3);
+
+INSERT INTO tags(tag_name) VALUES
+   ('adorable'),
+   ('cute'),
+   ('sunrise');
+
+INSERT INTO photo_tags (photo_id, tag_id) VALUES
+      (1, 2),
+      (2, 3),
+      (3, 2);
+
 
 
 SELECT * FROM users;

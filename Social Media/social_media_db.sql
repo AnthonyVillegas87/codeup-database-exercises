ad49c0586028a9e6dd12ca9cc8bff00d2149a960
Anthony Villegas <villegasanthony87@yahoo.com>
2022-07-27 14:51:24 -0500

    feat: photos table created for schema

--- Social Media/social_media_db.sql
+++ Social Media/social_media_db.sql
@@ -8,6 +8,13 @@ CREATE TABLE users(
 );
 DROP TABLE users;
 
+CREATE TABLE photos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 
 
 INSERT INTO users(username) VALUES
@@ -15,4 +22,10 @@ INSERT INTO users(username) VALUES
     ('BlueTheCat'),
     ('AlexanderTheGreat');
 
+INSERT INTO photos(image_url, user_id) VALUES
+   ('/alsjk76', 1),
+   ('/aldjn24', 2),
+   ('/iniaeuhfiquhe22', 2);
+
 SELECT * FROM users;
+SELECT * FROM photos;
